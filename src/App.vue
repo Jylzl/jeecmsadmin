d408a3a14b54e871653f1ba0fb4d5d7fc970be2d
lizilong <94648929@qq.com>
2019-02-22 18:07:43 +0800

    页面结果调整

--- src/App.vue
+++ src/App.vue
@@ -4,48 +4,68 @@
 		<BackToTop />
 	</div>
 </template>
-
 <script>
-import BackToTop from '@/components/BackToTop/index.vue'
-export default {
-	components: {
-		BackToTop
+	import BackToTop from '@/components/BackToTop/index.vue'
+
+	export default {
+		components: {
+			BackToTop
+		}
 	}
-}
 </script>
-
 <style lang="scss">
-@import url(./assets/css/rest.min.css);
+	@import url(./assets/css/rest.min.css);
 
-html,
-body {
-	height: 100%;
-}
+	html,
+	body {
+		height: 100%;
+	}
 
-#app {
-	height: 100%;
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-}
+	#app {
+		height: 100%;
+		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+	}
 
-.clearfix:after {
-	content: "";
-	display: block;
-	clear: both;
-	height: 0;
-}
+	@media (-webkit-max-device-pixel-ratio: 1) {
+		::-webkit-scrollbar-track-piece {
+			background-color: #ffffff;
+		}
 
-.clearfix {
-	zoom: 1;
-}
+		::-webkit-scrollbar {
+			width: 6px;
+			height: 6px;
+		}
 
-.fl {
-	float: left;
-}
+		::-webkit-scrollbar-thumb {
+			background-color: #c2c2c2;
+			background-clip: padding-box;
+			min-height: 28px;
+		}
 
-.fr {
-	float: right;
-}
+		::-webkit-scrollbar-thumb:hover {
+			background-color: #a0a0a0;
+		}
+	}
+
+	.clearfix:after {
+		content: "";
+		display: block;
+		clear: both;
+		height: 0;
+	}
+
+	.clearfix {
+		zoom: 1;
+	}
+
+	.fl {
+		float: left;
+	}
+
+	.fr {
+		float: right;
+	}
 </style>
