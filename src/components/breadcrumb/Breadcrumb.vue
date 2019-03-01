fef232ff939405dfa1ecb74a07ae3b03815ef09d
lizilong <94648929@qq.com>
2019-03-02 01:32:50 +0800

    内容页布局

--- src/components/breadcrumb/Breadcrumb.vue
+++ src/components/breadcrumb/Breadcrumb.vue
@@ -1,5 +1,5 @@
 <template>
-	<el-breadcrumb separator="/">
+	<el-breadcrumb separator-class="el-icon-d-arrow-right">
 		<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
 		<el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
 		<el-breadcrumb-item>活动列表</el-breadcrumb-item>
